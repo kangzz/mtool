7828b7a37f032506263708463ba5e06de0428400
kangzz <zhuang_it@163.com>
2016-12-26 18:41:19 +0800

    BooleanUtils

--- src/main/java/com/kangzz/mtool/util/BooleanUtils.java
+++ src/main/java/com/kangzz/mtool/util/BooleanUtils.java
@@ -9,14 +9,40 @@ import java.util.List;
  * 日期 ：2016-12-26 13:23:34
  */
 public class BooleanUtils {
+
+    private BooleanUtils(){
+
+    }
+    public static boolean isTrue(Boolean bool) {
+        return Boolean.TRUE.equals(bool);
+    }
+
+    public static boolean isNotTrue(Boolean bool) {
+        return !isTrue(bool);
+    }
+
+    public static boolean isFalse(Boolean bool) {
+        return Boolean.FALSE.equals(bool);
+    }
+
+    public static boolean isNotFalse(Boolean bool) {
+        return !isFalse(bool);
+    }
+
+    public static boolean toBoolean(Boolean bool) {
+        return bool != null && bool.booleanValue();
+    }
     /**
-     * 描述：所有判断必须是true 返回true 相当于 &&
+     * 描述：&& 封装
      * 作者 ：kangzz
-     * 日期 ：2016-12-26 13:22:41
+     * 日期 ：2016-12-26 18:39:45
      */
-    public static Boolean isAllTrue(Boolean... checkBooleans){
+    public static Boolean and(Boolean... checkBooleans){
         List<Boolean> checkBooleanList = (checkBooleans != null ? Arrays.asList(checkBooleans) : null);
-        for (int i = 0; CollectionUtil.isNotEmpty(checkBooleanList) && i < checkBooleanList.size(); i++) {
+        if(CollectionUtil.isNotEmpty(checkBooleanList)){
+           return false;
+        }
+        for (int i = 0;i < checkBooleanList.size(); i++) {
             if(!checkBooleanList.get(i)){
                 return false;
             }
@@ -24,18 +50,63 @@ public class BooleanUtils {
         return true;
     }
     /**
-     * 描述：只要有一个为true 返回true 相当于 ||
+     * 描述：&& 封装
      * 作者 ：kangzz
-     * 日期 ：2016-12-26 13:23:00
+     * 日期 ：2016-12-26 18:40:03
      */
-    public static Boolean hasTrue(Boolean... checkBooleans){
+    public static Boolean and(boolean... checkBooleans){
+        if(checkBooleans == null) {
+            return false;
+        } else if(checkBooleans.length == 0) {
+            return false;
+        } else {
+            boolean[] arr = checkBooleans;
+            int len = checkBooleans.length;
+            for (int i = 0; i < len; i++) {
+                if(!arr[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    /**
+     * 描述：|| 封装
+     * 作者 ：kangzz
+     * 日期 ：2016-12-26 18:40:30
+     */
+    public static Boolean or(Boolean... checkBooleans){
         List<Boolean> checkBooleanList = (checkBooleans != null ? Arrays.asList(checkBooleans) : null);
-        for (int i = 0; CollectionUtil.isNotEmpty(checkBooleanList) && i < checkBooleanList.size(); i++) {
+        if(CollectionUtil.isNotEmpty(checkBooleanList)){
+            return false;
+        }
+        for (int i = 0;i < checkBooleanList.size(); i++) {
             if(checkBooleanList.get(i)){
                 return true;
             }
         }
         return false;
+    }
+    /**
+     * 描述：|| 封装
+     * 作者 ：kangzz
+     * 日期 ：2016-12-26 18:40:30
+     */
+    public static Boolean or(boolean... checkBooleans){
+        if(checkBooleans == null) {
+            return false;
+        } else if(checkBooleans.length == 0) {
+            return false;
+        } else {
+            boolean[] arr = checkBooleans;
+            int len = checkBooleans.length;
+            for (int i = 0; i < len; i++) {
+                if(arr[i]) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
 }
